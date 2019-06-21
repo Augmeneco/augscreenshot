7f21966c9f2d3c29107adcf432109c33c7d0a776
cha14ka <taitor@yandex.ru>
2019-06-22 01:51:09 +0300

    Редизайн, кнопки и оптимизация кода

--- README.md
+++ README.md
@@ -7,9 +7,3 @@ AugScreenShoter - The program to create screenshots with the freeze of the scree
 - ```sudo apt install xclip imagemagick python3 python3-pip -y```
 - ```pip3 install pygame pillow```
 
-# Use:
-- Mouse1 - first point
-- Mouse2 - second point
-- Mouse1 - save screenshot
-
-If you pass the "save" argument as a parameter, the script will save the screenshot to the folder with images in the /home/%user%
--- augscreen.py
+++ augscreen.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-import os, time, sys
+import os, time
 from PIL import Image
 import contextlib
 with contextlib.redirect_stdout(None):
     import pygame
 from pygame.locals import *
 
-if 'save' in sys.argv:
-    save = True
-else:
-    save = False
 os.system('import -window root /tmp/augscreen.png')
 imgPIL = Image.open('/tmp/augscreen.png')
 w,h = imgPIL.size
@@ -26,49 +22,73 @@ newSurf.blit(img, (0,0))
 mainLoop = True
 click1 = False
 click2 = False
+draw = False
+activebutton = False
+
+bt_save = {'x':range(-1,-1),'y':range(-1,-1)}
+bt_buffer = {'x':range(-1,-1),'y':range(-1,-1)}
+
 while mainLoop:
 	for event in pygame.event.get():
 		if event.type == QUIT:
 				mainLoop = False
 	screen.fill((255,255,255))
 	screen.blit(newSurf, (0,0))
-	mouse_pose = pygame.mouse.get_pos()
-	if pygame.mouse.get_pressed()[0] == 1 and click1==False:
-		print('1 click on',mouse_pose)
+	
+	if pygame.mouse.get_pressed()[0] == 1 and click1 == False and draw == True and pygame.mouse.get_pos()[0] in bt_save['x'] and pygame.mouse.get_pos()[1] in bt_save['y']:
+		activebutton = True
+		cropped_img = imgPIL.crop(click1_pose+mouse_pose)
+		if 'Изображения' in os.listdir(os.getcwd()): cropped_img.save(os.getcwd()+'/Изображения/augscreen.png')
+		else: cropped_img.save(os.getcwd()+'/Pictures/augscreen.png')
+		mainLoop = False
+		
+	if pygame.mouse.get_pressed()[0] == 1 and click1 == False and draw == True and pygame.mouse.get_pos()[0] in bt_buffer['x'] and pygame.mouse.get_pos()[1] in bt_buffer['y']:
+		activebutton = True
+		cropped_img = imgPIL.crop(click1_pose+mouse_pose)
+		cropped_img.save('/tmp/augscreen.png')
+		os.system('xclip -selection clipboard -t image/png -i /tmp/augscreen.png;rm /tmp/augscreen.png')
+		mainLoop = False
+
+	if pygame.mouse.get_pressed()[0] == 1 and activebutton == False:
+		draw = True
+		mouse_pose = pygame.mouse.get_pos()
+
+	if pygame.mouse.get_pressed()[0] == 1 and click1==False and activebutton == False:
 		click1_pose = mouse_pose
 		click1 = True
-	if pygame.mouse.get_pressed()[2] == 1 and click1:
-		print('2 click on',mouse_pose)
-		click2_pose = mouse_pose
-		click2 = True
-	if click1 and click2 != True:
-		pygame.draw.circle(screen,[255,0,0], click1_pose,5)
-		pygame.draw.line(screen, [255,0,0], click1_pose, [mouse_pose[0],click1_pose[1]])
-		pygame.draw.line(screen, [255,0,0], [mouse_pose[0],click1_pose[1]], mouse_pose)
-		pygame.draw.line(screen, [255,0,0], click1_pose, [click1_pose[0],mouse_pose[1]])
-		pygame.draw.line(screen, [255,0,0], [click1_pose[0],mouse_pose[1]], mouse_pose)
-		pygame.draw.circle(screen,[255,0,0],mouse_pose,5)
-	if click1 and click2:
-		pygame.draw.circle(screen,[255,0,0], click1_pose,5)
-		pygame.draw.line(screen, [255,0,0], click1_pose, [click2_pose[0],click1_pose[1]])
-		pygame.draw.line(screen, [255,0,0], [click2_pose[0],click1_pose[1]], click2_pose)
-		pygame.draw.line(screen, [255,0,0], click1_pose, [click1_pose[0],click2_pose[1]])
-		pygame.draw.line(screen, [255,0,0], [click1_pose[0],click2_pose[1]], click2_pose)
-		pygame.draw.circle(screen,[255,0,0],click2_pose,5)
+		mouse_pose = click1_pose
+	if pygame.mouse.get_pressed()[0] == 0 and click1==True:
+		click1 = False
+	if draw:
+		pygame.draw.circle(screen,[105,105,105], click1_pose,5)
+		pygame.draw.line(screen, [105,105,105], click1_pose, [mouse_pose[0],click1_pose[1]])
+		pygame.draw.line(screen, [105,105,105], [mouse_pose[0],click1_pose[1]], mouse_pose)
+		pygame.draw.line(screen, [105,105,105], click1_pose, [click1_pose[0],mouse_pose[1]])
+		pygame.draw.line(screen, [105,105,105], [click1_pose[0],mouse_pose[1]], mouse_pose)
+		
+		pygame.draw.rect(screen, [105,105,105], [click1_pose[0],click1_pose[1]-30,90,25])
+		font = pygame.font.Font(None, 25)
+		text = font.render(str(mouse_pose[0]-click1_pose[0]).replace('-','')+'x'+str(mouse_pose[1]-click1_pose[1]).replace('-',''),True,[255,255,255])
+		screen.blit(text, [click1_pose[0],click1_pose[1]-28])
+		
+		pygame.draw.rect(screen, [105,105,105], [mouse_pose[0]+10,mouse_pose[1],60,25])
+		font = pygame.font.Font(None, 25)
+		text = font.render("буфер",True,[255,255,255])
+		screen.blit(text, [mouse_pose[0]+10,mouse_pose[1]])
+		bt_buffer['x'] = range(mouse_pose[0]+10,mouse_pose[0]+10+60)
+		bt_buffer['y'] = range(mouse_pose[1],mouse_pose[1]+60)
+		
+		pygame.draw.rect(screen, [105,105,105], [mouse_pose[0]+10,mouse_pose[1]+30,90,25])
+		font = pygame.font.Font(None, 25)
+		text = font.render("сохранить",True,[255,255,255])
+		screen.blit(text, [mouse_pose[0]+10,mouse_pose[1]+30])
+		bt_save['x'] = range(mouse_pose[0]+10,mouse_pose[0]+10+90)
+		bt_save['y'] = range(mouse_pose[1]+30,mouse_pose[1]+30+25)
+		
+		pygame.draw.circle(screen,[105,105,105],mouse_pose,5)
+	
+		
 	pygame.display.update()
-	if click1 and click2 and pygame.mouse.get_pressed()[0] == 1:
-		mainLoop = False
-		pygame.quit()
 	time.sleep(0.005)
 pygame.quit()
-print('Click1: ',click1_pose,'\nClick2: ',click2_pose)
-cropped_img = imgPIL.crop(click1_pose+click2_pose)
-if save:
-    if 'Изображения' in os.listdir(os.getcwd()):
-        cropped_img.save(os.getcwd()+'/Изображения/augscreen.png')
-    else:
-        cropped_img.save(os.getcwd()+'/Pictures/augscreen.png')
-else:
-    cropped_img.save('/tmp/augscreen.png')
-    os.system('xclip -selection clipboard -t image/png -i /tmp/augscreen.png;rm /tmp/augscreen.png')
-print('Сохранено в буфер')
+
